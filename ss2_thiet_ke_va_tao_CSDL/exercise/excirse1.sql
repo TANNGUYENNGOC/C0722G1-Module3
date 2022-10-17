6913dde5462709be2348d247510588c2dce568f5
TANNGUYENNGOC <nguyentantai1610@gmail.com>
2022-10-17 11:22:40 +0700

    upmodule3

--- ss2_thiet_ke_va_tao_CSDL/exercise/excirse1.sql
+++ ss2_thiet_ke_va_tao_CSDL/exercise/excirse1.sql
@@ -1,67 +1,73 @@
 create database quan_li_vat_tu;
 use quan_li_vat_tu;
 
-create table phieu_xuat(
-	so_phieu_xuat int primary key,
-    ngay_xuat datetime not null
+CREATE TABLE phieu_xuat (
+    so_phieu_xuat INT PRIMARY KEY,
+    ngay_xuat DATETIME NOT NULL
 );
 
-create TABLE vat_tu(
-	ma_vat_tu int primary key,
-    ten_vat_tu nvarchar(50)
+CREATE TABLE vat_tu (
+    ma_vat_tu INT PRIMARY KEY,
+    ten_vat_tu NVARCHAR(50)
 );
 
-create table chi_tiet_phieu_xuat(
-	don_gia_xuat int,
-    so_luong_xuat int,
-    so_phieu_xuat int,
-    ma_vat_tu int,
-    primary key(so_phieu_xuat, ma_vat_tu),
-    foreign key(ma_vat_tu) references vat_tu(ma_vat_tu),
-    foreign key(so_phieu_xuat) references phieu_xuat(so_phieu_xuat)
+CREATE TABLE chi_tiet_phieu_xuat (
+    don_gia_xuat INT,
+    so_luong_xuat INT,
+    so_phieu_xuat INT,
+    ma_vat_tu INT,
+    PRIMARY KEY (so_phieu_xuat , ma_vat_tu),
+    FOREIGN KEY (ma_vat_tu)
+        REFERENCES vat_tu (ma_vat_tu),
+    FOREIGN KEY (so_phieu_xuat)
+        REFERENCES phieu_xuat (so_phieu_xuat)
 );
 
-CREATE TABLE phieu_nhap(
-	so_phieu_nhap INT PRIMARY KEY,
+CREATE TABLE phieu_nhap (
+    so_phieu_nhap INT PRIMARY KEY,
     ngay_nhap DATETIME
 );
 
-CREATE TABLE chi_tiet_phieu_nhap(
-	don_gia_nhap INT,
+CREATE TABLE chi_tiet_phieu_nhap (
+    don_gia_nhap INT,
     so_luong_nhap INT,
-    ma_vat_tu int,
+    ma_vat_tu INT,
     so_phieu_nhap INT,
-    PRIMARY KEY(ma_vat_tu, so_phieu_nhap),
-    FOREIGN KEY(ma_vat_tu) REFERENCES vat_tu(ma_vat_tu),
-    FOREIGN KEY(so_phieu_nhap) REFERENCES phieu_nhap(so_phieu_nhap)
+    PRIMARY KEY (ma_vat_tu , so_phieu_nhap),
+    FOREIGN KEY (ma_vat_tu)
+        REFERENCES vat_tu (ma_vat_tu),
+    FOREIGN KEY (so_phieu_nhap)
+        REFERENCES phieu_nhap (so_phieu_nhap)
 );
 
-CREATE TABLE don_dat_hang(
-	so_dat_hang int PRIMARY KEY,
+CREATE TABLE don_dat_hang (
+    so_dat_hang INT PRIMARY KEY,
     ngay_dat_hang DATETIME
 );
 
-CREATE TABLE chi_tiet_don_dat_hang(
-	ma_vat_tu int,
-    so_dat_hang int,
-    PRIMARY KEY(ma_vat_tu, so_dat_hang),
-    FOREIGN KEY(ma_vat_tu) REFERENCES vat_tu(ma_vat_tu),
-    FOREIGN KEY(so_dat_hang) REFERENCES don_dat_hang(so_dat_hang)
+CREATE TABLE chi_tiet_don_dat_hang (
+    ma_vat_tu INT,
+    so_dat_hang INT,
+    PRIMARY KEY (ma_vat_tu , so_dat_hang),
+    FOREIGN KEY (ma_vat_tu)
+        REFERENCES vat_tu (ma_vat_tu),
+    FOREIGN KEY (so_dat_hang)
+        REFERENCES don_dat_hang (so_dat_hang)
 );
 
-CREATE TABLE nha_cung_cap(
-	ma_nha_cung_cap INT PRIMARY KEY,
+CREATE TABLE nha_cung_cap (
+    ma_nha_cung_cap INT PRIMARY KEY,
     ten_nha_cung_cap NVARCHAR(100),
     dia_chi NVARCHAR(100),
     so_dien_thoai VARCHAR(10)
 );
 
-CREATE TABLE cung_cap(
-	ma_nha_cung_cap INT,
+CREATE TABLE cung_cap (
+    ma_nha_cung_cap INT,
     so_dat_hang INT,
-    PRIMARY KEY(ma_nha_cung_cap, so_dat_hang),
-    FOREIGN KEY(ma_nha_cung_cap) REFERENCES nha_cung_cap(ma_nha_cung_cap),
-    FOREIGN KEY(so_dat_hang) REFERENCES don_dat_hang(so_dat_hang)
+    PRIMARY KEY (ma_nha_cung_cap , so_dat_hang),
+    FOREIGN KEY (ma_nha_cung_cap)
+        REFERENCES nha_cung_cap (ma_nha_cung_cap),
+    FOREIGN KEY (so_dat_hang)
+        REFERENCES don_dat_hang (so_dat_hang)
 );
-
-
--- ss2_thiet_ke_va_tao_CSDL/exercise/exercise2.sql
+++ ss2_thiet_ke_va_tao_CSDL/exercise/exercise2.sql
@@ -1,31 +1,34 @@
 CREATE DATABASE quan_li_ban_hang;
 USE quan_li_ban_hang;
 
-CREATE TABLE customer(
-	c_id INT PRIMARY KEY,
+CREATE TABLE customer (
+    c_id INT PRIMARY KEY,
     c_name NVARCHAR(50),
     c_age INT
 );
 
-CREATE TABLE `order`(
-	o_id INT PRIMARY KEY,
+CREATE TABLE `order` (
+    o_id INT PRIMARY KEY,
     c_id INT,
     o_date DATETIME,
     o_total_price INT,
-    FOREIGN KEY(c_id) REFERENCES customer(c_id)
+    FOREIGN KEY (c_id)
+        REFERENCES customer (c_id)
 );
 
-CREATE TABLE product(
-	p_id INT PRIMARY KEY,
+CREATE TABLE product (
+    p_id INT PRIMARY KEY,
     p_name NVARCHAR(50),
     p_price INT
 );
 
-CREATE TABLE order_detail(
-	o_id INT,
+CREATE TABLE order_detail (
+    o_id INT,
     p_id INT,
-    od_qty NVARCHAR (200),
-    PRIMARY KEY(o_id, p_id),
-    FOREIGN KEY(o_id) REFERENCES `order`(o_id),
-    FOREIGN KEY(p_id) REFERENCES product(p_id)
+    od_qty NVARCHAR(200),
+    PRIMARY KEY (o_id , p_id),
+    FOREIGN KEY (o_id)
+        REFERENCES `order` (o_id),
+    FOREIGN KEY (p_id)
+        REFERENCES product (p_id)
 );
